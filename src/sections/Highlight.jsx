b5c6427eae021bfc56df39a79765d9e51aed3076
jaykim1993 <tjdwnddl809@naver.com>
2026-04-13 14:50:24 +0900

    재배포1

--- src/sections/Highlight.jsx
+++ src/sections/Highlight.jsx
@@ -5,7 +5,7 @@ export default function Highlight() {
   return (
     <section id="home" className={styles.section}>
       <h1 className={styles.title}>Innovative Future</h1>
-      <p className={styles.description}>우리는 기술로 세상을 변화시킵니다.</p>
+      <p className={styles.description}>우리는 기술ss로 세상을 변화시킵니다.</p>
     </section>
   );
 }
